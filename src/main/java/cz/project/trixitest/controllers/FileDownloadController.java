ef865d5fa9ff08c9a043d39ef5ccdf5492d572e7
Arsen290 <arsen290@ukr.net>
2023-11-01 22:55:31 +0100

    Test

--- src/main/java/cz/project/trixitest/controllers/FileDownloadController.java
+++ src/main/java/cz/project/trixitest/controllers/FileDownloadController.java
@@ -1,6 +1,8 @@
 package cz.project.trixitest.controllers;
 
 
+import cz.project.trixitest.services.FileProcessingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 
@@ -15,19 +17,29 @@ import java.net.URL;
 
 @Controller
 public class FileDownloadController {
+    @Autowired
+    private FileProcessingService fileProcessingService;
+
     final String downloadUrl = "https://www.smartform.cz/download/kopidlno.xml.zip";
-    // Creat func to download zip file from certain URL
-    @GetMapping()
-    // Sets the HTTP response status code to 204 (No Content)
+
+    @GetMapping("/download") // Обратите внимание на URL-путь "/download"
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void downloadFile() throws IOException{
-        try (BufferedInputStream in = new BufferedInputStream(new URL(downloadUrl).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("kopidlno.xml.zip")) {
-            byte dataBuffer[] = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                fileOutputStream.write(dataBuffer, 0, bytesRead);
+    public void downloadFile() {
+        try {
+            URL url = new URL(downloadUrl);
+            try (BufferedInputStream in = new BufferedInputStream(url.openStream());
+                 FileOutputStream fileOutputStream = new FileOutputStream("kopidlno.xml.zip")) {
+                byte dataBuffer[] = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                    fileOutputStream.write(dataBuffer, 0, bytesRead);
+                }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    // Unzip
+    fileProcessingService.unpackingArchive();
+
     }
 }
